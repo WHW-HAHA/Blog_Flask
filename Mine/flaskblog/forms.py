5a45d9e76b6b5a590df068c55f0932d96e65585c
UniCorn <43483189+WHW-HAHA@users.noreply.github.com>
2019-12-17 15:52:11 +0100

    Updates: User profile updation

--- Mine/flaskblog/forms.py
+++ Mine/flaskblog/forms.py
@@ -1,5 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
@@ -19,7 +21,7 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
     
-    def validate_email(self, email):
+    def ds(self, email):
         user = User.query.filter_by(email = email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
@@ -32,3 +34,23 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user: # if the new user name already existed in the DB
+                raise ValidationError('The username is taken. Please take a new one.')
+
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email = email.data).first()
+            if user: # if the new user name already existed in the DB
+                raise ValidationError('The email address is taken. Please take a new one.')
+
--- Mine/flaskblog/models.py
+++ Mine/flaskblog/models.py
@@ -3,6 +3,11 @@ from flaskblog import db, login_manager
 from flask_login import UserMixin
 
 
+@login_manager.user_loader
+def load_user(user_id): # what this function doing?
+    return User.query.query.get(int(user_id))
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
