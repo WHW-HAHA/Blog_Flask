ddb937a003e99833fe4d04f466ca91f0bd71436c
UniCorn <43483189+WHW-HAHA@users.noreply.github.com>
2019-12-20 13:48:04 +0100

    Update: enable the post functionality

--- Mine/flaskblog/routes.py
+++ Mine/flaskblog/routes.py
@@ -1,11 +1,11 @@
-from flask import render_template, url_for, flash, redirect, request, abort 
+import os
+import secrets
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-import os
-import secrets
-from PIL import Image
 # Step 8
 
 # some test info
@@ -78,13 +78,14 @@ def logout():
 def save_picture(form_picture):
         random_hex = secrets.token_hex(8) # to return a 8 digits token key
         _, f_ext = os.path.splitext(form_picture.filename)
-        picture_fn = random_hex +f_ext
+        picture_fn = random_hex + f_ext
         picture_path =os.path.join(app.root_path, 'static/profile_pics', picture_fn)
         output_size = (125, 125) # 125^2 pixels
         image = Image.open(form_picture)
         image.thumbnail(output_size)
         image.save(picture_path)
         return picture_fn
+
 
 @app.route('/account', methods = ['GET', 'POST'])
 @login_required  # name of the method, the account page only accessable when user is log-in already
@@ -105,7 +106,56 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title = 'Account', image_file = image_file, form = form)
 
+@app.rute("/post/new", methods = ['GET', 'Post'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title = form.title.data, content = form.content.data,  author = current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title = 'New Post', form = form, legend = 'New Post ')
+
+@app.route("/post/<int:post_id>")
 def post(post_id):
-    post()
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title = post.title, post = post)
+
+
+@app.route("/post/<int:post_id>/update", methods = ['GET', 'POST'])
+@login_required
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403) # raise 403 error
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id = post_id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html',title = 'Update.post', form = form, legend = 'Update Post')
+
+@app.route("/post/<int:post_id>/delete", method = ['Post']) # delete is always a post action and not get action
+@login_required # name of the method
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
+
+
+
+
+
 
 
