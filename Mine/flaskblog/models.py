5b4a05234edf950bc0b71d92183bcaaea96a805a
UniCorn <43483189+WHW-HAHA@users.noreply.github.com>
2019-12-23 16:22:22 +0100

    updates

--- Mine/flaskblog/models.py
+++ Mine/flaskblog/models.py
@@ -17,7 +17,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
-    def get_reset_token(self, expires_sec = 1800):
+    def get_reset_token(self, expires_sec = 2000):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
 
