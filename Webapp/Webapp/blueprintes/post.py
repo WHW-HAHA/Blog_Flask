de64d410b51377ba744e4da158e3e065d9227409
UniCorn <43483189+WHW-HAHA@users.noreply.github.com>
2020-03-04 17:08:49 +0100

    Updates: post showcase page

--- Webapp/Webapp/blueprintes/post.py
+++ Webapp/Webapp/blueprintes/post.py
@@ -11,13 +11,14 @@ Naming standard:
     # 中文的话是需要特别注意的地方以及需要检查的地方
 """
 
-from flask import Blueprint
+from flask import Blueprint, render_template
+from Webapp.models import Post
 
 post_bp = Blueprint('post', __name__)
 
 @post_bp.route("/post/<int:post_id>") # post_id 在这个route函数被调用时传入
 def post(post_id):
     post = Post.query.get_or_404(post_id)
-    return render_template('post.html', title=post.title, post=post)
+    return render_template('post_content.html', title=post.title, post=post)
 
 
