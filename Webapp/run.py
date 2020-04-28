a9c86361ea629132ceda8d7a68e1296c81d72d20
UniCorn <43483189+WHW-HAHA@users.noreply.github.com>
2020-04-28 21:50:10 +0200

    Updates: styles

--- Webapp/run.py
+++ Webapp/run.py
@@ -11,15 +11,9 @@ Naming standard:
     # 中文的话是需要特别注意的地方以及需要检查的地方
 """
 import os
-from dotenv import load_dotenv
-
-# claim and use .env or .flaskenv as setting files
-#dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-#if os.path.exists(dotenv_path):
-#    load_dotenv(dotenv_path)
 
 from Webapp import create_app
 
 if __name__== '__main__':
     app = create_app()
-    app.run(debug = True)
+    app.run(debug = True, host='0.0.0.0', )
